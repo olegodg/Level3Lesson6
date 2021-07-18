77071d80b350a8c128f254e234211353bdaf71e0
OlegZhur <olegodg@yandex.ru>
2021-07-18 11:21:53 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/level3lesson6/MainApp.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.level3lesson6;
+
+public class MainApp {
+        public static void main(String[] args) {
+            final int[] array = pullOut(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+            System.out.println(array);
+            final boolean result = CheckArray(new int[]{1, 1, 1, 1, 1});
+            System.out.println(result);
+        }
+
+        public static int[] pullOut(int[] arrayInput) {
+            int[] backArray = new int[2];
+            if(arrayInput.length == 0) {
+                System.out.println("В массиве нет элементов");
+                return arrayInput;
+            }
+            for (int i = arrayInput.length - 3; i >= 0 ; i--) {
+                if(arrayInput[i] == 4) {
+                    System.arraycopy(arrayInput, (i+1), backArray,0,2);
+                    return backArray;
+                }
+            }
+            throw new RuntimeException("В массиве нет ни одной 4-ки после которой были бы числа");
+        }
+        public static boolean CheckArray(int [] in) {
+            boolean one = false;
+            boolean four = false;
+            for (int i: in) {
+                if(i != 1 && i!=4) throw new RuntimeException("Неверное значение");
+                if(i == 1) one = true;
+                if(i == 4) four = true;
+            }
+            return one && four;
+        }
+    }
